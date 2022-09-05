3efd9c1258d673861aa8799b8d94af4a21be0f65
JuanMaOrtizE <53711761+JuanMaOrtizE@users.noreply.github.com>
2022-09-05 17:10:41 -0500

    :+1:

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from classroom.grupo import Grupo
 if __name__ == "__main__":
     asignatura1 = Asignatura("Matematicas")
     asignatura2 = Asignatura("Castellano", "Salon 201")
-    grupo1 = Grupo()
-
-    print(asignatura1)
-    print(grupo1)
-    print(grupo1.grado)
+   
+    grupo1 = Grupo() 
+    
+    print(asignatura1)  #Matematicas remoto
+    print(grupo1)       #Grupo de estudiantes: grupo predeterminado
+    print(grupo1.grado) #Grado 12
 
     grupo2 = Grupo("Grupo 5", [], ["Alejandro", "Carlos"])
 
@@ -19,14 +20,14 @@ if __name__ == "__main__":
     grupo4.agregarAlumno("Santiago", ["Jaime", "Pedro"])
     grupo5.agregarAlumno("Javier")
 
-    print(grupo3.listadoAlumnos)
-    print(grupo4.listadoAlumnos)
-    print(grupo5.listadoAlumnos)
+    print(grupo3.listadoAlumnos)#['Kelly']
+    print(grupo4.listadoAlumnos)#['Jaime', 'Pedro', 'Santiago']
+    print(grupo5.listadoAlumnos)#['Javier']
 
     grupo3.listadoAsignaturas(as1="Ciencias", as2="Quimica", as3="Ingles")
-    print(len(grupo3._asignaturas))
+    print(len(grupo3._asignaturas))#3
 
     Grupo.asignarNombre("Grado 1")
-    print(Grupo.grado)
+    print(Grupo.grado)#Grado 1
     Grupo.asignarNombre()
-    print(Grupo.grado)
+    print(Grupo.grado)#Grado 6
